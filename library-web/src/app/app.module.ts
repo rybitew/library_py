f2ffce07f1a97fff469486d441b50cddb946961c
rybitew <rybitew@gmail.com>
2021-01-27 18:48:55 +0100

    optimizing imports

--- library-web/src/app/app.module.ts
+++ library-web/src/app/app.module.ts
@@ -1,23 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
 
 
-
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
-import { HomeComponent } from './components/home/home.component';
-import { BooksComponent } from './components/books/books.component';
-import { AccountComponent } from './components/account/account.component';
+import {AppComponent} from './app.component';
+import {NavMenuComponent} from './components/nav-menu/nav-menu.component';
+import {HomeComponent} from './components/home/home.component';
+import {BooksComponent} from './components/books/books.component';
+import {AccountComponent} from './components/account/account.component';
 import {MatTableModule} from '@angular/material/table';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import { BookDialogComponent } from './components/books/book-dialog/book-dialog.component';
+import {BookDialogComponent} from './components/books/book-dialog/book-dialog.component';
 import {MatDialogModule} from '@angular/material/dialog';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatDatepickerModule} from '@angular/material/datepicker';
--- library-web/src/app/components/home/home.component.ts
+++ library-web/src/app/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {BookDto} from '../../models/BookDto';
 
 @Component({
   selector: 'app-home',
