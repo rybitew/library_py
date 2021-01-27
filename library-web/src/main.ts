c21da411ca3b83c35f4d67bf056f44bc68b22fd6
rybitew <rybitew@gmail.com>
2021-01-27 18:32:59 +0100

    initial angular app config changes

--- library-web/src/main.ts
+++ library-web/src/main.ts
@@ -4,9 +4,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+export function getBaseUrl() {
+  return 'http://localhost:8000';
+}
+
+const providers = [
+  { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }
+];
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic(providers).bootstrapModule(AppModule)
+  .catch(err => console.log(err));
